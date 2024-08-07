db92d6509e4bc0eb244b0746b1cb5cd5b665ff03
= <=>
2024-08-07 19:37:35 -0300

    Exemplos Javascript variáveis

---
+++ Aula02/ex1.js
@@ -0,0 +1,8 @@
+const idade = 21;
+const nome = 'Sarah';
+
+console.log(`${idade} ${nome}`);
+
+const inf = nome + idade
+
+console.log(inf)
---
+++ Aula02/ex2.js
@@ -0,0 +1,3 @@
+var valor = 50.5;
+var num = 100;
+var a = true;
