194230600a54cf67fb4818cce594de1692bc71ce
sarahsilvap <sarahs-pereira@outlook.com>
2024-09-04 22:41:22 -0300

    exercicios aula 05

---
+++ Aula05/ex4-array.js
@@ -0,0 +1,16 @@
+notas =[8,9,7,6]
+console.log(notas)
+
+notas.pop()
+console.log(notas)
+
+notas.push(9.5)
+console.log(notas)
+notas.pop()
+console.log(notas)
+notas.pop()
+console.log(notas.pop())
+notas.push(8)
+console.log(notas)
+notas.push(7.5)
+console.log(notas)
